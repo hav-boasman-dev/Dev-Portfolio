6f8938ebf86d8417883be58928d2b666e9fc4eee
HavenBoasman (Home PC) <hav.boasman.dev@gmail.com>
2026-05-01 23:56:12 +1000

    Expanded upon project tab

--- src/app/components/ProjectsCard.tsx
+++ src/app/components/ProjectsCard.tsx
@@ -1,5 +1,9 @@
-import React from "react";
-import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
+"use client";
+import React, { useState, useRef, useTransition } from "react";
+import {
+  CodeBracketIcon,
+  ArrowsPointingOutIcon,
+} from "@heroicons/react/24/outline";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -7,17 +11,34 @@ const ProjectsCard = ({
   imgUrl,
   title,
   description,
+  descriptionExtended,
   gitUrl,
 }: {
   imgUrl: string;
   title: string;
   description: string;
+  descriptionExtended: string;
   gitUrl: string;
 }) => {
+  const [isFullscreen, setIsFullscreen] = useState(true);
+  const fullscreenToggle = () => {
+    setIsFullscreen((prev) => !prev);
+  };
+
   return (
-    <div>
+    <div
+      className={
+        isFullscreen
+          ? ""
+          : "absolute top-0 left-0 z-1000 col-span-3 row-span-2 float-left h-full w-full flex-none transform justify-self-start p-0 align-top"
+      }
+    >
       <div
-        className='group/project relative h-52 rounded-t-xl md:h-72'
+        className={
+          isFullscreen
+            ? "group/project relative h-52 rounded-t-xl md:h-72"
+            : "group/project relative h-102 rounded-t-xl md:h-72"
+        }
         style={{
           background: `url(${imgUrl})`,
           backgroundSize: "cover",
@@ -32,11 +53,25 @@ const ProjectsCard = ({
           >
             <CodeBracketIcon className='absolute top-1/8 left-1/2 h-10 w-10 -translate-x-1/2 transform cursor-pointer text-[#ADB7BE] group-hover/link:text-[#00e58e]'></CodeBracketIcon>
           </Link>
+          <button
+            onClick={fullscreenToggle}
+            className='group/link relative mr-2 hidden h-14 w-14 rounded-full border-2 border-[#ADB7BE] group-hover/project:flex hover:border-[#00e58e]'
+          >
+            <ArrowsPointingOutIcon className='absolute top-1/8 left-1/2 h-10 w-10 -translate-x-1/2 transform cursor-pointer text-[#ADB7BE] group-hover/link:text-[#00e58e]'></ArrowsPointingOutIcon>
+          </button>
         </div>
       </div>
-      <div className='rounded-b-xl bg-[#181818] px-4 py-6 text-white'>
+      <div
+        className={
+          isFullscreen
+            ? "rounded-b-xl bg-[#181818] px-4 py-6"
+            : "h-full w-full rounded-b-xl bg-[#181818] px-8 py-6 text-lg"
+        }
+      >
         <h5 className='mb-2 text-xl font-semibold'>{title}</h5>
-        <p className='text-[#ADB7BE]'>{description}</p>
+        <p className='text-[#ADB7BE]'>
+          {isFullscreen ? <p>{description}</p> : <p>{descriptionExtended}</p>}
+        </p>
       </div>
     </div>
   );
--- src/app/components/ProjectsSection.tsx
+++ src/app/components/ProjectsSection.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useTransition } from "react";
 import ProjectCard from "./ProjectsCard";
 import Image from "next/image";
 import { motion, useInView } from "framer-motion";
@@ -7,24 +7,30 @@ import { motion, useInView } from "framer-motion";
 const projectsData = [
   {
     id: 1,
-    title: "React Portfolio Website",
-    description:
-      "This portfolio was made using the next.js framework to learn modern web development pipelines while preparing for work.",
-    image: "/images/Projects/portfolioCard.png",
-    gitUrl: "https://github.com/hav-boasman-dev/dev-portfolio",
-  },
-  {
-    id: 2,
     title: "The Longest Dungeon",
     description:
       "A Unity based deck building dungeon crawler game exploring the themes of burnout, escapism and the cost of living as my universty capstone project",
+    descriptionExtended:
+      "A Unity based deck building dungeon crawler game exploring the themes of burnout, escapism and the cost of living as my universty capstone project </br> </br> The endearing pixel art facade of a dungeon is interrupted by modern day concepts that seem out of place. The game slowly reveals itself as you play with the intermittent addition of stressful new features until the facade is dropped entirely and we're met with the bleak reality of a young adult using escapsim to cope with their daily life. Through the lens of a card-based, day by day, dungeon crawler, The Longest Dungeon explores the themes of modern day burnout, mounting financial pressure and escapism.",
     image: "/images/Projects/theLongestDungeonCard.png",
     gitUrl: "https://github.com/hav-boasman-dev/TheLongDungeon",
   },
   {
     id: 2,
+    title: "React Portfolio Website",
+    description:
+      "This portfolio was made using the next.js framework to learn modern web development pipelines while preparing for work.",
+    descriptionExtended:
+      "This portfolio was made using the next.js framework to learn modern web development pipelines while preparing for work.",
+    image: "/images/Projects/portfolioCard.png",
+    gitUrl: "https://github.com/hav-boasman-dev/dev-portfolio",
+  },
+  {
+    id: 3,
     title: "Haiku and Hint Fiction",
     description:
+      "Two small environments based around the concepts of haiku and hint fiction while depicting two of my core memories",
+    descriptionExtended:
       "Two small environments based around the concepts of haiku and hint fiction while depicting two of my core memories",
     image: "/images/Projects/haikusAndHintFictionCard.png",
     gitUrl: "/",
@@ -46,7 +52,10 @@ const ProjectsSection = () => {
         <h2 className='mt-4 mb-10 text-center text-4xl font-bold'>
           MY PROJECTS / WORK
         </h2>
-        <ul ref={ref} className='grid gap-8 md:grid-cols-3 md:gap-12'>
+        <ul
+          ref={ref}
+          className='grid gap-8 md:grid-cols-3 md:grid-rows-2 md:gap-12'
+        >
           {projectsData.map((project, index) => (
             <motion.li
               key={index}
@@ -56,9 +65,9 @@ const ProjectsSection = () => {
               transition={{ duration: 0.3, delay: index * 0.4 }}
             >
               <ProjectCard
-                key={project.id}
                 title={project.title}
                 description={project.description}
+                descriptionExtended={project.descriptionExtended}
                 imgUrl={project.image}
                 gitUrl={project.gitUrl}
               />
